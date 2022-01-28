4c2af034fd1a8536c87ef584569cc64e1360e69f
harry-tian <tianh@uchicago.edu>
2022-01-27 19:12:35 -0600

    pdash_h and others

---
+++ model/resnt_args.py
@@ -0,0 +1,299 @@
+# -*- coding: utf-8 -*-
+import os, pickle
+import time
+import argparse
+import shutil
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import transforms, models
+import pytorch_lightning as pl
+from torchmetrics.functional.classification import auroc, stat_scores, average_precision, precision_recall_curve, auc
+from pytorch_lightning.loggers import WandbLogger
+import wandb
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class RESN(pl.LightningModule):
+
+    def __init__(self, verbose=False, **config_kwargs):
+        super().__init__()
+        self.save_hyperparameters()
+        self.train_pairwise_distance = torch.Tensor(pickle.load(open(self.hparams.train_pairwise_distance, "rb")), device=self.device)
+        self.valid_pairwise_distance = torch.Tensor(pickle.load(open(self.hparams.valid_pairwise_distance, "rb")), device=self.device)
+
+        self.feature_extractor = models.resnet18(pretrained=self.hparams.pretrained)
+        num_features = 1000
+
+        self.embed_dim = self.hparams.embed_dim
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.triplet_loss = nn.TripletMarginLoss()
+        self.fc = nn.Sequential(
+            nn.BatchNorm1d(num_features), nn.ReLU(), nn.Dropout(), nn.Linear(num_features, 256), 
+            nn.BatchNorm1d(256), nn.ReLU(), nn.Dropout(), nn.Linear(256, self.embed_dim)
+        )
+        self.classifier = nn.Sequential(
+            nn.BatchNorm1d(self.embed_dim), nn.ReLU(), nn.Dropout(), nn.Linear(self.embed_dim, 1)
+        )
+
+        if verbose: 
+            self.summarize()
+
+
+    def metrics(self, prob, target, threshold=0.5):
+        pred = (prob >= threshold).long()
+        tp, fp, tn, fn, sup = stat_scores(pred, target, ignore_index=0)
+        if 0 < sup < len(target):
+            precision, recall, _ = precision_recall_curve(pred, target)
+            auprc = auc(recall, precision)
+        m = {}
+        m['pred'] = pred
+        m['auc'] = auroc(prob, target) if 0 < sup < len(target) else None
+        m['acc'] = (tp + tn) / (tp + tn + fp + fn)
+        m['tpr'] = tp / (tp + fn)
+        m['tnr'] = tn / (tn + fp)
+        m['ppv'] = tp / (tp + fp)
+        m['f1'] = 2 * tp / (2 * tp + fp + fn)
+        m['ap'] = average_precision(prob, target)
+        m['auprc'] = auprc if 0 < sup < len(target) else None
+        return m
+
+    def embed(self, x):
+        embeds = self.feature_extractor(x)
+        embeds = self.fc(embeds)
+        return embeds
+
+    def forward(self, x, i):
+        z = self.embed(x)
+        logits = self.classifier(z)
+        pairwise = self.train_pairwise_distance[i][:, i]
+        comb = torch.combinations(torch.range(0, len(z)-1).long(), r=3)
+        triplet_idx = []
+        for c in comb:
+            anchor, pos, neg = c
+            # print(pairwise.shape, anchor, pos)
+            if pairwise[anchor, pos] > pairwise[anchor, neg]:
+                triplet_idx.append((anchor, neg, pos))
+            else:
+                triplet_idx.append((anchor, pos, neg))
+        triplet_idx = torch.Tensor(triplet_idx).long()
+        # print(triplet_idx[:,0], triplet_idx[:,1], triplet_idx[:,2], triplet_idx[:,0].dtype)
+        triplets = (z[triplet_idx[:,0]], z[triplet_idx[:,1]], z[triplet_idx[:,2]])
+        return logits, triplets
+
+    def training_step(self, batch, batch_idx):
+        x, y, i = batch
+        logits, triplets = self(x, i)
+        prob = torch.sigmoid(logits)
+        loss = self.criterion(logits, y.type_as(logits).unsqueeze(1))
+        triplet_loss = self.triplet_loss(*triplets)
+        total_loss = loss + triplet_loss / (triplet_loss + 1)
+        with torch.no_grad():
+            m = self.metrics(prob, y.unsqueeze(1))
+        self.log('train_loss', loss, sync_dist=True)
+        self.log('train_triplet_loss', triplet_loss, sync_dist=True)
+        self.log('train_total_loss', total_loss, sync_dist=True)
+        self.log('train_acc', m['acc'], prog_bar=True, sync_dist=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y, i = batch
+        logits = self.classifier(self.embed(x))
+        prob = torch.sigmoid(logits)
+        loss = self.criterion(logits, y.type_as(logits).unsqueeze(1))
+        m = self.metrics(prob, y.unsqueeze(1))
+        self.log('valid_loss', loss, sync_dist=True)
+        self.log('valid_acc', m['acc'], prog_bar=True, sync_dist=True)
+        self.log('valid_auc', m['auc'], prog_bar=True, sync_dist=True)
+        self.log('valid_sensitivity', m['tpr'], sync_dist=True)
+        self.log('valid_specificity', m['tnr'], sync_dist=True)
+        self.log('valid_precision', m['ppv'], sync_dist=True)
+        self.log('valid_f1', m['f1'], sync_dist=True)
+        self.log('valid_ap', m['ap'], sync_dist=True)
+        self.log('valid_auprc', m['auprc'], sync_dist=True)
+        return {'valid_loss': loss, 'valid_auc': m['auc']}
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        return optimizer
+    
+    def parse_augmentation(self):
+        affine = {}
+        affine["degrees"] = self.hparams.rotate
+        if self.hparams.translate > 0: 
+            translate = self.hparams.translate
+            affine["translate"] = (translate, translate)
+        if self.hparams.scale > 0: 
+            scale = self.hparams.scale
+            affine["scale"] = (1 - scale, 1 + scale)
+        if self.hparams.shear > 0:
+            shear = self.hparams.shear
+            affine["shear"] = (-shear, shear, -shear, shear)
+        transform = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            transforms.RandomHorizontalFlip(self.hparams.horizontal_flip),
+            transforms.RandomVerticalFlip(self.hparams.vertical_flip),
+            transforms.RandomAffine(**affine)
+        ])
+        return transform
+
+    def train_dataloader(self):
+        ImageWithIndices = dataset_with_indices(torchvision.datasets.ImageFolder)
+        dataset = ImageWithIndices(
+            self.hparams.train_dir, transform=self.parse_augmentation()
+            )
+        dataloader = torch.utils.data.DataLoader(
+            dataset, 
+            batch_size=self.hparams.train_batch_size, 
+            num_workers=self.hparams.dataloader_num_workers, 
+            drop_last=True, shuffle=True)
+        return dataloader
+
+    def val_dataloader(self):
+        val_transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        ImageWithIndices = dataset_with_indices(torchvision.datasets.ImageFolder)
+        dataset = ImageWithIndices(
+            self.hparams.valid_dir, transform=val_transform
+            )
+        dataloader = torch.utils.data.DataLoader(
+            dataset, 
+            batch_size=len(dataset), 
+            num_workers=self.hparams.dataloader_num_workers, 
+            drop_last=False, shuffle=False)
+        return dataloader
+
+    @staticmethod
+    def add_model_specific_args(parser, root_dir):
+        parser.add_argument("--train_pairwise_distance", default=None, type=str, required=True)
+        parser.add_argument("--valid_pairwise_distance", default=None, type=str, required=True)
+        parser.add_argument("--pretrained", action="store_true")
+        parser.add_argument("--embed_dim", default=10, type=int, help="Embedding size")
+        parser.add_argument('--kernel', type=str, default='gaussian', help='hparam for kernel [guassian|laplace|invquad]')
+        parser.add_argument('--gamma', type=float, default=1.0, help='hparam for kernel')
+        parser.add_argument('--eps', type=float, default=1e-12, help='label smoothing factor for learning')
+        parser.add_argument("--horizontal_flip", default=0, type=float)
+        parser.add_argument("--vertical_flip", default=0, type=float)
+        parser.add_argument("--rotate", default=0, type=int)
+        parser.add_argument("--translate", default=0, type=float)
+        parser.add_argument("--scale", default=0, type=float)
+        parser.add_argument("--shear", default=0, type=float)
+        return parser
+
+def dataset_with_indices(cls):
+
+    def __getitem__(self, index):
+        data, target = cls.__getitem__(self, index)
+        return data, target, index
+
+    return type(cls.__name__, (cls,), {
+        '__getitem__': __getitem__,
+    })
+
+def train(
+    model:RESN,
+    args: argparse.Namespace,
+    early_stopping_callback=False,
+    extra_callbacks=[],
+    checkpoint_callback=None,
+    logging_callback=None,
+    **extra_train_kwargs
+    ):
+
+    # init model
+    odir = Path(model.hparams.output_dir)
+    odir.mkdir(parents=True, exist_ok=True)
+    log_dir = Path(os.path.join(model.hparams.output_dir, 'logs'))
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+    # build logger
+    ## WandB logger
+    experiment = wandb.init(
+        mode=args.wandb_mode, 
+        group=args.wandb_group
+    )
+    logger = WandbLogger(
+        project="imagenet_bm",
+        experiment=experiment
+    )
+
+    # add custom checkpoints
+    ckpt_path = os.path.join(
+        args.output_dir, logger.version, "checkpoints",
+    )
+    if checkpoint_callback is None:
+        checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            dirpath=ckpt_path, filename="{epoch}-{valid_loss:.2f}", monitor="valid_loss", mode="min", save_last=True, save_top_k=3, verbose=True
+        )
+
+    train_params = {}
+    train_params["max_epochs"] = args.max_epochs
+    if args.gpus == -1 or args.gpus > 1:
+        train_params["distributed_backend"] = "ddp"
+
+    trainer = pl.Trainer.from_argparse_args(
+        args,
+        auto_select_gpus=True,
+        weights_summary=None,
+        callbacks=extra_callbacks + [checkpoint_callback],
+        logger=logger,
+        **train_params,
+    )
+
+    if args.do_train:
+        trainer.fit(model)
+        # save best model to `best_model.ckpt`
+        target_path = os.path.join(ckpt_path, 'best_model.ckpt')
+        print(f"Copy best model from {checkpoint_callback.best_model_path} to {target_path}.")
+        shutil.copy(checkpoint_callback.best_model_path, target_path)
+
+    return trainer
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpus", default=0, type=int)
+    parser.add_argument("--seed", default=42, type=int)
+    parser.add_argument("--max_epochs", default=10, type=int)
+    parser.add_argument("--learning_rate", default=1e-4, type=float)
+    parser.add_argument("--train_batch_size", default=16, type=int)
+    parser.add_argument("--eval_batch_size", default=64, type=int)
+    parser.add_argument("--dataloader_num_workers", default=4, type=int)
+    parser.add_argument("--train_dir", default=None, type=str, required=True)
+    parser.add_argument("--valid_dir", default=None, type=str, required=True)
+    parser.add_argument("--output_dir", default=None, type=str, required=True)
+    parser.add_argument("--wandb_group", default=None, type=str)
+    parser.add_argument("--wandb_mode", default="online", type=str)
+    parser.add_argument("--do_train", action="store_true")
+    RESN.add_model_specific_args(parser, os.getcwd())
+    args = parser.parse_args()
+    print(args)
+
+    pl.seed_everything(args.seed)
+
+    if args.output_dir is None:
+        args.output_dir = os.path.join(
+            "./results",
+            f"{__name__}_{time.strftime('%Y%m%d_%H%M%S')}",
+        )
+        os.makedirs(args.output_dir)
+    
+    dict_args = vars(args)
+    model = RESN(**dict_args)
+    trainer = train(model, args)
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -2,14 +2,17 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from sklearn import neighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, accuracy_score
 from sklearn.svm import SVC, LinearSVC
 from aix360.algorithms.protodash import ProtodashExplainer
+from sklearn.metrics.pairwise import euclidean_distances
 from torchvision import transforms
 import tste
 import pickle
+import matplotlib.pyplot as plt
 
 def get_tste(distance_matrix, triplets_fname, tste_fname, no_dims=2, max_iter=1000):
     triplets = []
@@ -59,7 +62,6 @@ def get_knn_score(k, data, index, metric="auc", weights="uniform"):
         score = knc.score(x_valid, y_valid)
     return score
 
-
 def get_svm_score(k, data, index, metric=METRIC, linear=LINEAR):
     X_train, y_train, X_valid, y_valid = data
     svc = LinearSVC(random_state=42) if linear else SVC(probability=True, random_state=42)
@@ -83,7 +85,6 @@ def get_full_score(data, k_range):
     f_scores_knn = np.array(f_scores_knn)
     f_score_svm = get_svm_score(k, data, list(range(len(data[0]))))
     return f_scores_knn, f_score_svm
-
 
 def get_random_score(data, k_range, m_range, n_trials=100):
     X_train, y_train, X_valid, y_valid = data
@@ -138,12 +139,50 @@ def get_protodash_score(data, k_range, m_range):
     p_scores_svm = np.array(p_scores_svm).reshape(len(k_range), len(m_range))
     return p_scores_knn, p_scores_svm
 
-def get_nn(index, samples, m=1):
+def get_1nn_lpips(index, samples):
+    target = samples[index].copy()
+    sample_index = {sample:i for i,sample in enumerate(target)}
+    target.sort()
+    return sample_index[target[1]]
+
+def get_1nn(index, samples):
+    if len(samples) < 2:
+        return 0
+    # print(samples.shape)
+    # print(samples)
+    dist = euclidean_distances(samples)
+    nn = np.argsort(dist[index])[1]
+
+    return nn
+
+def get_nns(index, samples, m=1):
     dist = euclidean_distances(samples)
     neighbors = []
     for i in index:
         neighbors.append(np.argsort(dist[i])[1:m+1])
     return np.array(neighbors)
+
+def get_nn_triplets(index, triplets, m=1):
+    neighbors = {}
+    for triplet in triplets:
+        if triplet[0] == index:
+            pos = triplet[1]
+            if pos not in neighbors:
+                neighbors[pos] = 1 
+            else:
+                neighbors[pos] += 1
+
+    max_count = 0
+    for value in neighbors.values():
+        if value > max_count:
+            max_count = value
+
+    nns = []
+    for key, value in neighbors.items():
+        if value == max_count:
+            nns.append(key)
+
+    return np.array(nns)
 
 def prototype_knn(data, proto_idx, k_range, m_range):
     scores = []
@@ -152,6 +191,147 @@ def prototype_knn(data, proto_idx, k_range, m_range):
             scores.append(get_knn_score(k, data, proto_idx[m]))
 
     return np.array(scores).reshape(len(k_range), len(m_range))
+
+if True:
+    SMALL_SIZE = 10
+    MEDIUM_SIZE = 15
+    BIGGER_SIZE = 20
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+def vis_data(x_train, y_train, x_valid, y_valid, title, save=False):
+    x_all = np.concatenate((x_train, x_valid))
+    y_all = np.concatenate((y_train, y_valid))
+    classes = np.unique(y_train)
+    subtitles = ["all", "train", "valid"]
+    fig, ax = plt.subplots(1, len(subtitles), figsize=(24, 6))
+
+    for i, data in enumerate([(x_all, y_all), (x_train, y_train),(x_valid, y_valid)]):
+        x, y = data
+        for c in classes:
+            c_idx = np.where(y==c)[0]
+            ax[i].scatter(x[c_idx][:,0], x[c_idx][:,1])
+
+        ax[i].legend(['non-cancer','cancer'])
+        ax[i].set_title("{}".format(subtitles[i]))
+    
+    utils.normalize_xylim(ax)
+    fig.suptitle(title)
+    if save:
+        plt.savefig(f"{title}.png", dpi=300)
+
+def vis_data_all(x, y, title, save=False):
+    classes = np.unique(y)
+    fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+
+    for i, data in enumerate([(x,y),(x,y)]):
+        x, y = data
+        for c in classes:
+            c_idx = np.where(y==c)[0]
+            ax[i].scatter(x[c_idx][:,0], x[c_idx][:,1])
+
+        ax[i].legend(['non-cancer','cancer'])
+    
+    utils.normalize_xylim(ax)
+    fig.suptitle(title)
+    if save:
+        plt.savefig(f"{title}.png", dpi=300)
+
+def vis_proto(x_train, y_train,train_proto_idx, title, save=False, order=False, eps=0, x_valid=None, y_valid=None,valid_proto_idx=None):
+    classes = np.unique(y_train)
+    fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+    # for i, data in enumerate([(x_train, y_train),(x_valid, y_valid)]):
+    for i, data in enumerate([(x_train, y_train)]):
+        x, y = data
+        for c in classes:
+            c_idx = np.where(y==c)[0]
+            ax[i].scatter(x[c_idx][:,0], x[c_idx][:,1])
+        ax[i].legend(['non-cancer','cancer'])
+        ax[i].set_title(title)
+        # ax[i].set_title("{}.{}.{}".format(model, name, "train" if i == 0 else "valid"))
+
+    for j, c in enumerate(classes):
+        train_proto = x_train[[int(i) for i in train_proto_idx if y_train[i] == c]]
+        ax[0].scatter(train_proto[:,0], train_proto[:,1], s=300, c=f"C{str(j)}", marker='^', linewidths=1, edgecolors='k') 
+        # valid_proto = x_valid[[int(i) for i in valid_proto_idx if y_valid[i] == c]]
+        # ax[1].scatter(valid_proto[:,0], valid_proto[:,1], s=300, c=f"C{str(j)}",marker='^', linewidths=1, edgecolors='k') 
+
+    for o, i in enumerate(train_proto_idx):
+        proto = x_train[i]
+        c = plt.Circle(proto, radius=eps, fill=False,lw=0.75)
+        
+        if order:
+            ax[0].text(proto[0],proto[1],str(o), size="xx-large", weight="bold", c="0")
+        ax[0].add_patch(c)
+
+    if save:
+        plt.savefig(f"{title}.png", dpi=300)
+
+def vis_triplets(x_train, y_train, triplet_dict, title, save=False):
+    classes = np.unique(y_train)
+    fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+    for i, data in enumerate([(x_train, y_train)]):
+        x, y = data
+        for c in classes:
+            c_idx = np.where(y==c)[0]
+            ax[i].scatter(x[c_idx][:,0], x[c_idx][:,1])
+        
+        ax[i].legend(['non-cancer','cancer'])
+        ax[i].set_title("{}".format("train" if i == 0 else "valid"))
+    
+    for i, triplet in enumerate(triplet_dict):
+        anchor, pos, neg = triplet
+        anchor = x_train[anchor]
+        pos = x_train[pos]
+        neg = x_train[neg]
+        ax[0].scatter(anchor[0], anchor[1], s=300, c='0.5', marker='^', linewidths=1, edgecolors='k') 
+        ax[0].scatter(pos[0], pos[1], s=300, c='0', marker='^', linewidths=1, edgecolors='k') 
+        ax[0].scatter(neg[0], neg[1], s=300, c='1', marker='^', linewidths=1, edgecolors='k') 
+        ax[0].text(anchor[0], anchor[1], str(i), c='0', size="xx-large", weight="bold") 
+        ax[0].text(pos[0], pos[1], str(i), c='0', size="xx-large", weight="bold") 
+        ax[0].text(neg[0], neg[1], str(i), c='0', size="xx-large", weight="bold") 
+
+    # utils.normalize_xylim(ax)
+    fig.suptitle(title)
+    if save:
+        plt.savefig(f"{title}.png", dpi=300)
+
+def vis_proto_2(x_train, y_train, proto_idx1, proto_idx2, title, save=False, order=False, eps=0):
+    classes = np.unique(y_train)
+    fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+    for i, data in enumerate([(x_train, y_train)]):
+        x, y = data
+        for c in classes:
+            c_idx = np.where(y==c)[0]
+            ax[i].scatter(x[c_idx][:,0], x[c_idx][:,1])
+        ax[i].legend(['non-cancer','cancer'])
+        ax[i].set_title(title)
+
+        protos1 = x_train[proto_idx1]
+        protos2 = x_train[proto_idx2]
+        ax[i].scatter(protos1[:,0], protos1[:,1], s=300, c = 'g', marker='^', linewidths=1, edgecolors='k') 
+        ax[i].scatter(protos2[:,0], protos2[:,1], s=300, c = 'r', marker='^', linewidths=1, edgecolors='k') 
+    if save:
+        plt.savefig(f"{title}.png", dpi=300)
+    
+def vis_knn(k_range, m_range, scores, legend, title, save=False):
+    lw = 3
+    fig, ax = plt.subplots(1, len(k_range), figsize=(24,6), sharey=True)
+    for k in range(len(k_range)):
+        for score in scores:
+            ax[k].plot(m_range, score[k], lw=lw)
+
+        ax[k].legend(legend, loc='lower right')
+        ax[k].set_title('K={}'.format(k_range[k]))
+    fig.suptitle(title)
+
+    if save:
+        plt.savefig(f"{title}.png", dpi=300)
 
 def normalize_xylim(ax):
     min_x0 = np.inf
