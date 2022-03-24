75d466963670eb30a137e4fcf8258e79d8975080
mrsata <liuhanrick@gmail.com>
2022-03-23 22:00:37 -0500

    Update selection algo: rename triplet_greedy and signature

--- algorithms/selection.py
+++ algorithms/selection.py
@@ -7,33 +7,33 @@ from copy import deepcopy
 
 skey = lambda k: tuple(sorted(k))
 
-def get_topk(scores, visits, topk, type="count", confidence=0.01, verbose=False):
+def get_topk(scores, visits, topk, metric="count", confidence=0.01, verbose=False):
     topk = np.min([len(scores), topk])
-    if type == "count":
+    if metric == "count":
         keys = heapq.nlargest(topk, scores, key=scores.__getitem__)
         weights = [scores[k] for k in keys]
-    elif type == "acc":
-        acc = {b: scores[b] / (visits[b] + 1e-10) for b in scores}
+    if metric == "acc":
+        acc = {b: scores[b] / (visits[b] if visits[b] > 0 else 0) for b in scores}
         keys = heapq.nlargest(topk, acc, key=acc.__getitem__)
         weights = [acc[k] for k in keys]
-    elif type == "lcb":
+    if metric == "lcb":
         t = sum(visits.values())
-        acc = {b: scores[b] / (visits[b] + 1e-10) for b in scores}
+        acc = {b: scores[b] / (visits[b] if visits[b] > 0 else 0) for b in scores}
         lcb = {b: acc[b] - confidence * np.sqrt(t / visits[b] if visits[b] > 0 else 0) for b in scores}
         if verbose: 
             print({b: lcb[b] for b in heapq.nlargest(3, lcb, key=lcb.__getitem__)})
         keys = heapq.nlargest(topk, lcb, key=lcb.__getitem__)
         weights = [lcb[k] for k in keys] 
-    elif type == "ucb":
+    if metric == "ucb":
         t = sum(visits.values())
-        acc = {b: scores[b] / (visits[b] + 1e-10) for b in scores}
+        acc = {b: scores[b] / (visits[b] if visits[b] > 0 else 0) for b in scores}
         ucb = {b: acc[b] + confidence * np.sqrt(t / visits[b] if visits[b] > 0 else 0) for b in scores}
         if verbose: 
             print({b: ucb[b] for b in heapq.nlargest(3, ucb, key=ucb.__getitem__)})
         keys = heapq.nlargest(topk, ucb, key=ucb.__getitem__)
         weights = [ucb[k] for k in keys] 
     if verbose: 
-        print(keys[0], weights[0], topk)
+        print(keys[0], "{:.4f}".format(weights[0]), topk)
     return keys, weights
 
 
@@ -46,7 +46,7 @@ def get_candidates_with_label(scores, visits, labels):
     return candidate_scores, candidate_visits
 
 
-def select(embeds, m, triplets, labels=None, topk=10, verbose=False):
+def tripet_greedy(embeds, m, triplets, labels=None, topk=10, metric="count", verbose=False):
     if m < 3: 
         raise Exception("Cannot select less than 2 examples!")
     
@@ -80,9 +80,9 @@ def select(embeds, m, triplets, labels=None, topk=10, verbose=False):
     curr_visits = deepcopy(visits)
     if labels is not None:
         cand_scores, cand_visits = get_candidates_with_label(scores, visits, labels)
-        beam, w = get_topk(cand_scores, cand_visits, topk, type="count", verbose=verbose)
+        beam, w = get_topk(cand_scores, cand_visits, topk, metric=metric, verbose=verbose)
     else:
-        beam, w = get_topk(curr_scores, curr_visits, topk, type="count", verbose=verbose)
+        beam, w = get_topk(curr_scores, curr_visits, topk, metric=metric, verbose=verbose)
     for _ in range(3, m+1):
         new_scores = defaultdict(lambda: 0)
         new_visits = deepcopy(new_scores)
@@ -101,5 +101,5 @@ def select(embeds, m, triplets, labels=None, topk=10, verbose=False):
                         new_visits[key] += visits[skey([j, k])]
         curr_scores = new_scores
         curr_visits = new_visits
-        beam, w = get_topk(curr_scores, curr_visits, topk, type="count", verbose=verbose)
+        beam, w = get_topk(curr_scores, curr_visits, topk, metric=metric, verbose=verbose)
     return beam[0], w[0]
