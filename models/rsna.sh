c9c2a2cfedc31e2008510394b91828e0b178606c
harry-tian <tianh@uchicago.edu>
2022-04-05 23:23:57 -0500

    pushed .sh files

--- models/rsna.sh
+++ models/rsna.sh
@@ -10,23 +10,25 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20000
-#SBATCH --nodelist=aa002
-  
+#SBATCH --nodelist=aa001
+
+   
 python resn_args.py \
   --wandb_mode=online \
   --wandb_entity=ai-driven-tutorial \
-  --wandb_project=baselines \
-  --wandb_group=chest_xray \
-  --wandb_name=bs=32 \
-  --train_dir=/net/scratch/tianh-shared/chest_xray/train \
-  --valid_dir=/net/scratch/tianh-shared/chest_xray/valid \
-  --test_dir=/net/scratch/tianh-shared/chest_xray/test \
-  --num_class=4 \
-  --transform=xray \
+  --wandb_project=nih \
+  --wandb_group=10classes \
+  --wandb_name=test \
+  --train_dir=/net/scratch/tianh-shared/NIH/pathologies/train \
+  --valid_dir=/net/scratch/tianh-shared/NIH/pathologies/valid \
+  --test_dir=/net/scratch/tianh-shared/NIH/pathologies/valid \
+  --num_class=10 \
   --embed_dim=10 \
-  --max_epochs=200 \
-  --train_batch_size=32 \
-  --learning_rate=1e-4 \
   --pretrained \
+  --max_epochs=200 \
+  --train_batch_size=128 \
+  --learning_rate=1e-4 \
+  --transform=xray \
   --do_train \
   --do_test
+  
