9032aa08636c4a0d03dda5dbb07c80189f54c123
mrsata <liuhanrick@gmail.com>
2022-05-13 03:31:03 -0500

    Add weighted knn decision support

--- evals/embed_evals.py
+++ evals/embed_evals.py
@@ -14,22 +14,23 @@ def syn_evals(z_train, y_train, z_test, y_test, syn_x_train, syn_x_test, weights
     evals = {}
     
     NINO = get_NINO(z_train, y_train, z_test, y_test, k)
+    ds_dist = decision_support_with_dist if k==1 else knn_decision_support_with_dist
     if dist is not None:
-        NINO_ds_acc, NINO_ds_err = decision_support_with_dist(dist, NINO, y_train, y_test)
+        NINO_ds_acc, NINO_ds_err = ds_dist(dist, NINO, y_train, y_test)
     else:
         NINO_ds_acc, NINO_ds_err = decision_support(syn_x_train, y_train, syn_x_test, y_test, NINO, weights, powers)
     evals['NINO_ds_acc'], evals['NINO_ds_err'] = NINO_ds_acc, NINO_ds_err
     
     rNINO = get_rNINO(z_train, y_train, z_test, y_test, k)
     if dist is not None:
-        rNINO_ds_acc, rNINO_ds_err = decision_support_with_dist(dist, rNINO, y_train, y_test)
+        rNINO_ds_acc, rNINO_ds_err = ds_dist(dist, rNINO, y_train, y_test)
     else:
         rNINO_ds_acc, rNINO_ds_err = decision_support(syn_x_train, y_train, syn_x_test, y_test, rNINO, weights, powers)
     evals['rNINO_ds_acc'], evals['rNINO_ds_err'] = rNINO_ds_acc, rNINO_ds_err
 
     NIFO = get_NIFO(z_train, y_train, z_test, y_test, k)
     if dist is not None:
-        NIFO_ds_acc, NIFO_ds_err = decision_support_with_dist(dist, NIFO, y_train, y_test)
+        NIFO_ds_acc, NIFO_ds_err = ds_dist(dist, NIFO, y_train, y_test)
     else:
         NIFO_ds_acc, NIFO_ds_err = decision_support(syn_x_train, y_train, syn_x_test, y_test, NIFO, weights, powers)
     evals['NIFO_ds_acc'], evals['NIFO_ds_err'] = NIFO_ds_acc, NIFO_ds_err
@@ -177,6 +178,22 @@ def decision_support_with_dist(dist, examples, y_train, y_test):
             err.append([test_idx, examples_idx[0], examples_idx[1]])
             continue
         y_pred = y_train[examples_idx[np.argmin(dist[test_idx][examples_idx])]]
+        if y_pred == y_test[test_idx]: correct += 1
+        else: err.append([test_idx, examples_idx[0], examples_idx[1]])
+    return correct/len(y_test), err
+
+def knn_decision_support_with_dist(dist, examples, y_train, y_test):
+    correct = 0
+    err = []
+    for test_idx, examples_idx in enumerate(examples):       
+        examples_idx = examples_idx.flatten()
+        if len(np.unique(examples_idx)) < len(examples_idx): ## signal of error in rNINO
+            err.append([test_idx, examples_idx[0], examples_idx[1]])
+            continue
+        dists = dist[test_idx][examples_idx]
+        y_pred = y_train[examples_idx]
+        w = 1 / dists
+        y_pred = np.bincount(y_pred, w).argmax()
         if y_pred == y_test[test_idx]: correct += 1
         else: err.append([test_idx, examples_idx[0], examples_idx[1]])
     return correct/len(y_test), err
